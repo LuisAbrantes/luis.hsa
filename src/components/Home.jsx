39ca45315f55be1318f365b73f67fb85be671513
Luis Henrique dos Santos Abrantes <luis.hsa@gmail.com>
2024-12-16 18:43:13 -0300

    Update Home component text for improved clarity and consistency

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,11 @@
 const Home = () => {
     return (
         <div className="home-section bg-dark-primary text-dark-text">
-            <h1 className="text-4xl font-bold mb-4">HACK IS ALL YOU NEED !!</h1>
+            <h1 className="text-4xl font-bold mb-4">HACK is all you need !!</h1>
             <h2 className="name">Luis Henrique Abrantes</h2>
             <h3 className="title">Software Engineer & Tech Enthusiast</h3>
             <p className="introduction">
-                Passionate software engineer with expertise in full-stack
+                Passionate software engineering with expertise in full-stack
                 development and a strong focus on creating efficient, scalable
                 solutions. Experienced in modern web technologies and committed
                 to writing clean, maintainable code. Always eager to tackle new
