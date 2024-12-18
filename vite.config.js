161c22caa6c44c31edc9f916d0cbb79855176d4f
Luis Henrique dos Santos Abrantes <luis.hsa@gmail.com>
2024-12-18 16:07:16 -0300

    Add server configuration to Vite for network access and specify port

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/luis.hsa",
-})
+    plugins: [react()],
+    base: '/luis.hsa',
+    server: {
+        host: '0.0.0.0', // Permite que o servidor seja acessado por outros dispositivos na rede
+        port: 5173 // Porta onde o servidor vai rodar
+    }
+});
